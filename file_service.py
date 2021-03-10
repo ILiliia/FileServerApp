4ab529d61f9fb18a3e1378088bf21a0736c3d7c5
Liliia <kirpichenko.lili@gmail.com>
2021-03-10 12:33:21 +0200

    File Server updates

--- file_service.py
+++ file_service.py
@@ -1,19 +1,23 @@
 from utils import *
 import os
+import time
+
 dir = os.environ.get("FILE_DIR_PATH")
 file_name = random_string()
 file_path = os.path.join(dir, file_name)
 
 
 def create_file(name=file_path):
-    with open(name, 'w'):
+    with open(name, 'w') as f:
+        f.write(time.asctime())
         print("File created")
 
 
 def read_file_content(_file=file_path):
     with open(_file, 'r') as f:
-        f.read()
         print("File opened for reading")
+        content = f.read()
+        print(content)
 
 
 def delete_file(file=file_path):
--- main.py
+++ main.py
@@ -8,5 +8,3 @@ if __name__ == '__main__':
     options, args = parser.parse_known_args()
 
     os.environ['FILE_DIR_PATH'] = options.file_dir
-
-create_file()
